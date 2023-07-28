2f7957558bd3ed4f43d4225960c5864b0cdb76bf
George Salt <solanikg3@gmail.com>
2023-07-28 18:01:09 +0300

    Создал функцию для сохранения информации о пользователях в json

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ def get_data(filepath="users.json"):
     with open(filepath, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
+
+
+def save_data(data, filepath="users.json"):
+    with open(filepath, "w", encoding="utf-8") as file:
+        json.dump(data, file)

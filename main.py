99aefb7eca216379e7e46165296db61bdc3dc83e
George Salt <solanikg3@gmail.com>
2023-07-29 05:55:10 +0300

    Добавил обработчик стартовой команды

--- main.py
+++ main.py
@@ -19,3 +19,15 @@ def get_data(filepath="users.json"):
 def save_data(data, filepath="users.json"):
     with open(filepath, "w", encoding="utf-8") as file:
         json.dump(data, file)
+
+
+@BOT.message_handler(commands=["start"])
+def start(message):
+    BOT.send_message(
+        message.chat.id,
+        "Добро пожаловать в меню сотрудников компании Company!\n\nВыберите действие."
+    )
+
+
+if __name__ == "__main__":
+    BOT.infinity_polling()
